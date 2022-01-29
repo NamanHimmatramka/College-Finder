30a83e8f85aaa56c84834f98a647e471736656c7
Aniket Kudtarkar <aniketkud22@gmail.com>
2022-01-30 01:06:51 +0530

    College Info Fragment

--- app/src/main/java/com/example/collegefinder/CollegeInfoActivity.java
+++ app/src/main/java/com/example/collegefinder/CollegeInfoActivity.java
@@ -1,14 +1,41 @@
 package com.example.collegefinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CollegeInfoActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_college_info);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.info:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.college_info_frame_layout, new CollegeInfoFragment()).commit();
+                        return true;
+
+                    case R.id.map:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.college_info_frame_layout, new CollegeMapFragment()).commit();
+                        return true;
+                }
+
+
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/collegefinder/CollegeMapFragment.java
@@ -0,0 +1,79 @@
+package com.example.collegefinder;
+
+import android.Manifest;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+
+public class CollegeMapFragment extends Fragment implements OnMapReadyCallback {
+
+    public CollegeMapFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_college_map, container, false);
+
+        checkMyPermission(view);
+
+        boolean isPermissionGranted = false;
+        if(isPermissionGranted) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getParentFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+
+        return view;
+    }
+
+    private void checkMyPermission(View view) {
+        Dexter.withContext(view.getContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                boolean isPermissionGranted = true;
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).check();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
