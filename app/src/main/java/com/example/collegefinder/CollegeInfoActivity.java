1a6985f8b71f8c92beff99cab2e1c040f6e15389
Naman Himmatramka <himmatramka2003@gmail.com>
2022-01-30 00:21:09 +0530

    College Info UI Collapsing Toolbar

---
+++ app/src/main/java/com/example/collegefinder/CollegeInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.collegefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollegeInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_college_info);
+    }
+}
--- app/src/main/java/com/example/collegefinder/MainActivity.java
+++ app/src/main/java/com/example/collegefinder/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -17,43 +20,53 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
     boolean isPermissionGranted;
+    Button tryx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkMyPermission();
+        tryx = findViewById(R.id.tryx);
+        tryx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CollegeInfoActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        if(isPermissionGranted){
-            SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-        }
+//        checkMyPermission();
+//
+//        if(isPermissionGranted){
+//            SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+//            mapFragment.getMapAsync(this);
+//        }
     }
 
-    private void checkMyPermission() {
-        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                Toast.makeText(MainActivity.this, "Perimssion Given", Toast.LENGTH_LONG).show();
-                isPermissionGranted = true;
-            }
-
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-                Toast.makeText(MainActivity.this, "Perimssion Denied", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-
-            }
-        }).check();
-    }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-
-    }
+//    private void checkMyPermission() {
+//        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+//            @Override
+//            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+//                Toast.makeText(MainActivity.this, "Perimssion Given", Toast.LENGTH_SHORT).show();
+//                isPermissionGranted = true;
+//            }
+//
+//            @Override
+//            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+//                Toast.makeText(MainActivity.this, "Perimssion Denied", Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+//
+//            }
+//        }).check();
+//    }
+//
+//    @Override
+//    public void onMapReady(@NonNull GoogleMap googleMap) {
+//
+//    }
 }
